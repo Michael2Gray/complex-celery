8c88754f0ad163fa901509f0e4c8784cf5d04c83
Michael Gray <michael2.gray@gmail.com>
2022-01-14 12:06:03 +0000

    Setup Testing env

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './app';
+
+import './app/index.css';
+
+import { App } from './app/app';
 
 ReactDOM.render(
   <React.StrictMode>
