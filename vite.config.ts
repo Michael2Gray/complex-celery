3cf673ae9fcf3641a2de45eb02f96718cdca482c
Michael Gray <michael2.gray@gmail.com>
2022-02-04 15:11:12 +0000

    Add version to vite config and index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, UserConfigExport } from 'vite';
+import VitePluginHtmlEnv from 'vite-plugin-html-env';
 
 const config: UserConfigExport = {
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginHtmlEnv({
+      VITE_PACKAGE_VERSION: process.env.npm_package_version,
+    }),
+  ],
 };
 
 // https://vitejs.dev/config/
